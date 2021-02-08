64c16eeefa50d2fa96a5aad3cad2d718a58f97b1
Mustafa Tayyip BAYRAM <60510780+mutabay@users.noreply.github.com>
2021-02-08 04:21:13 +0100

    Add files via upload

---
+++ Lesson-12/Enums/src/enums/Enums.java
@@ -0,0 +1,20 @@
+
+package enums;
+
+    enum Level{
+        LOW,
+        MEDIUM,
+        HIGH,
+    }
+
+public class Enums {
+
+    public static void main(String[] args) {
+        
+        Level level = Level.LOW;
+        System.out.println(level);
+
+
+    }
+    
+}
